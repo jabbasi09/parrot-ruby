5a5798cc7d22cf513f66a63e1685cf0ed2e60728
Junaid Abbasi <my.github.juni.abbasi@icloud.com>
2020-10-21 16:42:16 -0500

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,8 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot(defaultPhrase = "Squawk!")
+    puts defaultPhrase
+    return defaultPhrase
+end
+parrot("Pretty bird!")
